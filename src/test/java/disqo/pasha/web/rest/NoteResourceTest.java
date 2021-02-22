c29a53273d8f3e1263524c8e844f5bb231fc4122
Pasha <pasha.gharibi@gmail.com>
2021-02-22 17:44:51 +0400

    ETL - Note API test

--- src/test/java/disqo/pasha/web/rest/NoteResourceTest.java
+++ src/test/java/disqo/pasha/web/rest/NoteResourceTest.java
@@ -1,38 +1,238 @@
 package disqo.pasha.web.rest;
 
-import org.junit.jupiter.api.AfterEach;
+import disqo.pasha.NotesApplication;
+import disqo.pasha.TestUtil;
+import disqo.pasha.domain.Note;
+import disqo.pasha.repository.NoteRepository;
+import disqo.pasha.service.NoteService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 
-class NoteResourceTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(classes = NotesApplication.class)
+@AutoConfigureMockMvc
+@WithMockUser
+public class NoteResourceTest {
+    private static final String DEFAULT_TITLE = "AAAAAAAAAA";
+    private static final String UPDATED_TITLE = "BBBBBBBBBB";
+
+    private static final String DEFAULT_NOTE = "AAAAAAAAAA";
+    private static final String UPDATED_NOTE = "BBBBBBBBBB";
+
+    private static final LocalDate DEFAULT_CREATE_TIME = LocalDate.ofEpochDay(0L);
+    private static final LocalDate UPDATED_CREATE_TIME = LocalDate.now(ZoneId.systemDefault());
+
+    private static final LocalDate DEFAULT_LAST_UPDATE_TIME = LocalDate.ofEpochDay(0L);
+    private static final LocalDate UPDATED_LAST_UPDATE_TIME = LocalDate.now(ZoneId.systemDefault());
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+    @Autowired
+    private NoteService noteService;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private MockMvc restNoteMockMvc;
+
+    private Note note;
+
+    /**
+     * Create an entity for this test.
+     * <p>
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Note createEntity(EntityManager em) {
+        Note note = new Note(DEFAULT_TITLE)
+                .note(DEFAULT_NOTE)
+                .createTime(DEFAULT_CREATE_TIME)
+                .lastUpdateTime(DEFAULT_LAST_UPDATE_TIME);
+        return note;
+    }
+
+    /**
+     * Create an updated entity for this test.
+     * <p>
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Note createUpdatedEntity(EntityManager em) {
+        Note note = new Note(UPDATED_TITLE)
+                .note(UPDATED_NOTE)
+                .createTime(UPDATED_CREATE_TIME)
+                .lastUpdateTime(UPDATED_LAST_UPDATE_TIME);
+        return note;
+    }
 
     @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+    public void initTest() {
+        note = createEntity(em);
     }
 
     @Test
-    void createNote() {
+    @Transactional
+    public void createNote() throws Exception {
+        int databaseSizeBeforeCreate = noteRepository.findAll().size();
+        // Create the Note
+        restNoteMockMvc.perform(post("/api/notes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtil.convertObjectToJsonBytes(note)))
+                .andExpect(status().isCreated());
+
+        // Validate the Note in the database
+        List<Note> noteList = noteRepository.findAll();
+        assertThat(noteList).hasSize(databaseSizeBeforeCreate + 1);
+        Note testNote = noteList.get(noteList.size() - 1);
+        assertThat(testNote.getTitle()).isEqualTo(DEFAULT_TITLE);
+        assertThat(testNote.getNote()).isEqualTo(DEFAULT_NOTE);
+        assertThat(testNote.getCreateTime()).isEqualTo(DEFAULT_CREATE_TIME);
+        assertThat(testNote.getLastUpdateTime()).isEqualTo(DEFAULT_LAST_UPDATE_TIME);
     }
 
     @Test
-    void updateNote() {
+    @Transactional
+    public void createNoteWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = noteRepository.findAll().size();
+
+        // Create the Note with an existing ID
+        note.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restNoteMockMvc.perform(post("/api/notes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtil.convertObjectToJsonBytes(note)))
+                .andExpect(status().isBadRequest());
+
+        // Validate the Note in the database
+        List<Note> noteList = noteRepository.findAll();
+        assertThat(noteList).hasSize(databaseSizeBeforeCreate);
+    }
+
+
+    @Test
+    @Transactional
+    public void getAllNotes() throws Exception {
+        // Initialize the database
+        noteRepository.saveAndFlush(note);
+
+        // Get all the noteList
+        restNoteMockMvc.perform(get("/api/notes?sort=id,desc"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.[*].id").value(hasItem(note.getId().intValue())))
+                .andExpect(jsonPath("$.[*].title").value(hasItem(DEFAULT_TITLE)))
+                .andExpect(jsonPath("$.[*].note").value(hasItem(DEFAULT_NOTE)))
+                .andExpect(jsonPath("$.[*].createTime").value(hasItem(DEFAULT_CREATE_TIME.toString())))
+                .andExpect(jsonPath("$.[*].lastUpdateTime").value(hasItem(DEFAULT_LAST_UPDATE_TIME.toString())));
     }
 
     @Test
-    void getAllNotes() {
+    @Transactional
+    public void getNote() throws Exception {
+        // Initialize the database
+        noteRepository.saveAndFlush(note);
+
+        // Get the note
+        restNoteMockMvc.perform(get("/api/notes/{id}", note.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id").value(note.getId().intValue()))
+                .andExpect(jsonPath("$.title").value(DEFAULT_TITLE))
+                .andExpect(jsonPath("$.note").value(DEFAULT_NOTE))
+                .andExpect(jsonPath("$.createTime").value(DEFAULT_CREATE_TIME.toString()))
+                .andExpect(jsonPath("$.lastUpdateTime").value(DEFAULT_LAST_UPDATE_TIME.toString()));
     }
 
     @Test
-    void getNote() {
+    @Transactional
+    public void getNonExistingNote() throws Exception {
+        // Get the note
+        restNoteMockMvc.perform(get("/api/notes/{id}", Long.MAX_VALUE))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    void deleteNote() {
+    @Transactional
+    public void updateNote() throws Exception {
+        // Initialize the database
+        noteService.save(note);
+
+        int databaseSizeBeforeUpdate = noteRepository.findAll().size();
+
+        // Update the note
+        Note updatedNote = noteRepository.findById(note.getId()).get();
+        // Disconnect from session so that the updates on updatedNote are not directly saved in db
+        em.detach(updatedNote);
+        updatedNote.setTitle(UPDATED_TITLE);
+        updatedNote.note(UPDATED_NOTE)
+                .createTime(UPDATED_CREATE_TIME)
+                .lastUpdateTime(UPDATED_LAST_UPDATE_TIME);
+
+        restNoteMockMvc.perform(put("/api/notes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtil.convertObjectToJsonBytes(updatedNote)))
+                .andExpect(status().isOk());
+
+        // Validate the Note in the database
+        List<Note> noteList = noteRepository.findAll();
+        assertThat(noteList).hasSize(databaseSizeBeforeUpdate);
+        Note testNote = noteList.get(noteList.size() - 1);
+        assertThat(testNote.getTitle()).isEqualTo(UPDATED_TITLE);
+        assertThat(testNote.getNote()).isEqualTo(UPDATED_NOTE);
+        assertThat(testNote.getCreateTime()).isEqualTo(UPDATED_CREATE_TIME);
+        assertThat(testNote.getLastUpdateTime()).isEqualTo(UPDATED_LAST_UPDATE_TIME);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingNote() throws Exception {
+        int databaseSizeBeforeUpdate = noteRepository.findAll().size();
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restNoteMockMvc.perform(put("/api/notes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtil.convertObjectToJsonBytes(note)))
+                .andExpect(status().isBadRequest());
+
+        // Validate the Note in the database
+        List<Note> noteList = noteRepository.findAll();
+        assertThat(noteList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    public void deleteNote() throws Exception {
+        // Initialize the database
+        noteService.save(note);
+
+        int databaseSizeBeforeDelete = noteRepository.findAll().size();
+
+        // Delete the note
+        restNoteMockMvc.perform(delete("/api/notes/{id}", note.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        // Validate the database contains one less item
+        List<Note> noteList = noteRepository.findAll();
+        assertThat(noteList).hasSize(databaseSizeBeforeDelete - 1);
     }
 }
